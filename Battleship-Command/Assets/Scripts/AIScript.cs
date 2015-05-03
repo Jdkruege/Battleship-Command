c26080c51b07c43d9ad47c4d2751331d2bbdc203
nasutton <nasutton@iastate.edu>
2015-05-02 23:30:43 -0500

    Prefabs, Scripts
    Added Prefabs for ships, planes and guns. They need to be updated with
    completed scripts. Added Scripts for shooting, and an abstract
    superclass for AI involving guns.

---
+++ Battleship-Command/Assets/Scripts/AIScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class AIScript : MonoBehaviour
+{
+    public abstract bool CanShoot();
+
+    public abstract void Shoot(GameObject ammo);
+}
