772062447ecad088d7e8b2e13b7ecbb4111ab39f
Monkooky <buisinessmonkey@gmail.com>
2015-05-03 19:11:32 -0500

    Basic player control

---
+++ Battleship-Command/Assets/Scripts/Game Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    Vector3 pastPosition;
+    Transform joystick;
+	// Use this for initialization
+	void Start () {
+        joystick = transform.FindChild("MobileSingleStickControl").gameObject.transform.FindChild("MobileJoystick").gameObject.transform;
+        pastPosition = joystick.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        steer();
+	}
+
+    private void steer()
+    {
+
+
+        transform.position += ( joystick.transform.position-pastPosition ) / 500;
+    }
+}
