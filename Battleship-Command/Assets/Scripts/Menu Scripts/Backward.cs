0aaf5f5ee3ec3714d73eee56d1f2987269b48d7e
Jdkruege <Jdkruege@users.noreply.github.com>
2015-05-04 09:57:57 -0500

    Committing so I can access later. The play screen is close to being dynamic.

---
+++ Battleship-Command/Assets/Scripts/Menu Scripts/Backward.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Backward : MonoBehaviour {
+
+    public GameObject paperHolder;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (paperHolder.GetComponent<PaperHolder>().index == 0)
+        {
+            Color color = GetComponent<SpriteRenderer>().color;
+            color.a = 0.5f;
+            GetComponent<SpriteRenderer>().color = color;
+        }
+        else
+        {
+            Color color = GetComponent<SpriteRenderer>().color;
+            color.a = 255;
+            GetComponent<SpriteRenderer>().color = color;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        paperHolder.GetComponent<PaperHolder>().BackwardAPage();
+    }
+}
