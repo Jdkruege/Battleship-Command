734ad80a523c7fe3ab3bf027bce3ba0dab5ffcb6
Monkooky <buisinessmonkey@gmail.com>
2015-05-04 01:44:04 -0500

    Completed targetting, made Player singleton
    Made Player a singleton class- call Player.getPlayer() to obtain the
    scene's Player.
    
    Targetting is now complete. it has been moved to the player class. Units
    can have multiple reticles on them at a time- this can be substituted
    for a less lazy method if time permits

--- Battleship-Command/Assets/Scripts/Game Scripts/IsTargetable.cs
+++ Battleship-Command/Assets/Scripts/Game Scripts/IsTargetable.cs
@@ -8,31 +8,15 @@ public class IsTargetable : MonoBehaviour
 {
     private GameObject controller;
 
-    public int targetCount;
 
     public void Start()
     {
-        targetCount = 0;
         controller = GameObject.Find("Controller");
     }
 
     public void OnMouseDown()
     {
-        if (targetCount < 3)
-        {
-            targetCount++;
-            if (transform.FindChild("Reticle") == null)
-            {
-                GameObject reticle = Instantiate(controller.GetComponent<Resources>().reticle);
-                reticle.transform.parent = this.transform;
-                reticle.transform.position = this.transform.position;
-                reticle.transform.name = "Reticle";
-            }
-        }
-        else
-        {
-            targetCount = 0;
-            Destroy(transform.FindChild("Reticle").gameObject);
-        }
+        Player.getPlayer().targetShip(this);
+        
     }
 }
--- Battleship-Command/Assets/Scripts/Game Scripts/Player.cs
+++ Battleship-Command/Assets/Scripts/Game Scripts/Player.cs
@@ -1,25 +1,37 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
+    private static Player playerinstance;
 
     Vector3 pastPosition;
     Transform joystick;
     float currentrot;
     Rigidbody2D body;
-	// Use this for initialization
+
+    private int targetCount;
+    LinkedList<IsTargetable> targetQueue;
+    private GameObject controller;
+
+    // Use this for initialization
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         joystick = transform.FindChild("MobileSingleStickControl").gameObject.transform.FindChild("MobileJoystick").gameObject.transform;
         pastPosition = joystick.transform.position;
         currentrot = 0;
+
+        targetQueue = new LinkedList<IsTargetable>();
+        targetCount = 0;
+
+        controller = GameObject.Find("Controller");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         steer();
 	}
+
 
     private void steer()
     {
@@ -28,9 +40,43 @@ public class Player : MonoBehaviour {
 
 
         float rotvel = (joystick.transform.position - pastPosition).x/200 ;
-        currentrot += rotvel;
+        currentrot -= rotvel;
         body.MoveRotation(currentrot);
 
     }
 
+    public static Player getPlayer()
+    {
+        if (playerinstance == null)
+        {
+            playerinstance = GameObject.FindObjectOfType<Player>();
+        }
+        return playerinstance;
+    }
+
+
+    internal void targetShip(IsTargetable target)
+    {
+        //Add another reticle on top of the targetted object
+        GameObject reticle = Instantiate(controller.GetComponent<Resources>().reticle);
+        reticle.transform.parent = target.transform;
+        reticle.transform.position = target.transform.position;
+        reticle.transform.name = "Reticle";
+        targetQueue.AddLast(target);
+
+        // if we have more than three targets, forget the oldest, destroying one of it's reticles
+        if (targetCount >= 3)
+        {
+            IsTargetable detarget = targetQueue.First.Value;
+            Destroy(detarget.transform.FindChild("Reticle").gameObject);
+            targetQueue.RemoveFirst();
+        }
+        else
+        {
+            targetCount++;
+        }
+
+
+
+    }
 }
