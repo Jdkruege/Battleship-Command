c5c56ea20004e4d69bab0e2e119d7d1162d6d643
Monkooky <buisinessmonkey@gmail.com>
2015-05-04 23:51:33 -0500

    Enemy spawning
    Enemies now spawn every 180 frames.

---
+++ Battleship-Command/Assets/Scripts/Game Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour {
+
+    public Spawn[] spawnpoints;
+
+    public int spawntime; //How long between spawns
+    private int spawndelay; //How long till next spawn
+
+	// Use this for initialization
+	void Start () {
+        spawndelay = spawntime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (spawndelay > 0){
+            spawndelay--;
+        }else{
+            int whichSpawn = Random.Range(0, spawnpoints.Length - 1);
+            spawnpoints[whichSpawn].spawn();
+            spawndelay = spawntime;
+        }
+	
+	}
+}
---
+++ Battleship-Command/Assets/Scripts/Game Scripts/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject toSpawn;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void spawn()
+    {
+        GameObject newship = Instantiate(toSpawn);
+    }
+
+}
